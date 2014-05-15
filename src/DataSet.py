e5a222b582c92b4b5b7afa07474fbdbf95293e81
Frank Dorssers <frank.dorssers@gmail.com>
2014-05-15 23:37:58 +0200

    Fix pylint errors

--- src/DataSet.py
+++ src/DataSet.py
@@ -1,12 +1,13 @@
 '''
 Created on May 15, 2014
 
-@author: Frank Dorsser
+@author: Frank Dorssers
 '''
 
 from os import listdir
 from os.path import isfile, join
 import numpy as np
+
 
 class DataSet(object):
     '''
@@ -38,61 +39,63 @@ class DataSet(object):
         self.strategy_matrix = self.generate_strategy_matrix(fdict)
         self.strategies = self.get_strategy_file_names()
         self.problems = fdict.keys()
-        
-    def is_relevant_strategy(self,filename):
-        whitelist = ['protokoll_G','protokoll_H','protokoll_U']
-        return any(map(filename.startswith,whitelist))
 
-    def get_strategy_file_names(self,path='../data/TESTRUNS_PEGASUS/'):
-        return [f for f in listdir(path) if isfile(join(path,f)) and self.is_relevant_strategy(f)]
-    
-    def initialize_dict_with_problems(self,f,path):
+    def is_relevant_strategy(self, filename):
+        whitelist = ['protokoll_G', 'protokoll_H', 'protokoll_U']
+        return any(map(filename.startswith, whitelist))
+
+    def get_strategy_file_names(self, path='../data/TESTRUNS_PEGASUS/'):
+        return [f for f in listdir(path) if
+                isfile(join(path, f)) and self.is_relevant_strategy(f)]
+
+    def initialize_dict_with_problems(self, sfile, path):
         sdict = {}
-        with open(path+f,'r') as IS:
+        with open(path+sfile, 'r') as inputstream:
             firstline = True
-            for line in IS:
+            for line in inputstream:
                 if firstline:
                     firstline = False
                 else:
-                    sdict[line.split()[0]] = [[],[]]
+                    sdict[line.split()[0]] = [[], []]
         return sdict
 
-    def load_strategies(self,path='../data/TESTRUNS_PEGASUS/'):
-        fs = self.get_strategy_file_names()
-        sdict = self.initialize_dict_with_problems(fs[0],path)
-        for f in fs:
+    def load_strategies(self, path='../data/TESTRUNS_PEGASUS/'):
+        sfiles = self.get_strategy_file_names()
+        sdict = self.initialize_dict_with_problems(sfiles[0], path)
+        for sfile in sfiles:
             firstline = True
-            with open(path+f,'r') as IS:
-                for line in IS:
+            with open(path+sfile, 'r') as inputstream:
+                for line in inputstream:
                     if firstline:
                         firstline = False
                     else:
                         sline = line.split()
-                        if(sline[1] != 'T'):
+                        if sline[1] != 'T':
                             sdict[sline[0]][0].append(-1)
                         else:
                             sdict[sline[0]][0].append(float(sline[2]))
         return sdict
-                    
-    def load_features(self,fdict,path='../data/'):
-        f = 'pegasusFeatures'
-        with open(path+f,'r') as IS:
+
+    def load_features(self, fdict, path='../data/'):
+        ffile = 'pegasusFeatures'
+        with open(path+ffile, 'r') as inputstream:
             firstline = True
-            for line in IS:
+            for line in inputstream:
                 if firstline:
                     firstline = False
                 else:
                     tmp = (line.strip()).split('#')
-                    fdict[tmp[0].split('/')[2]][1] = [float(x) for x in tmp[1].split(',')]
+                    key = tmp[0].split('/')[2]
+                    fdict[key][1] = [float(x) for x in tmp[1].split(',')]
         return fdict
-    
-    def generate_feature_matrix(self,fdict):
+
+    def generate_feature_matrix(self, fdict):
         total_features = []
         for key in fdict.keys():
             total_features.append(fdict[key][1])
         return np.array(total_features)
-        
-    def generate_strategy_matrix(self,fdict):
+
+    def generate_strategy_matrix(self, fdict):
         total_strategies = []
         for key in fdict.keys():
             total_strategies.append(fdict[key][0])
