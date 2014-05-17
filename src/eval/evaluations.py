2d50cdc5fc39927937612270158aa78a2c4cbe11
dkuehlwein <daniel.kuehlwein@gmail.com>
2014-05-17 14:14:00 +0200

    Basics for eval

---
+++ src/eval/evaluations.py
@@ -0,0 +1,19 @@
+'''
+Created on May 17, 2014
+
+@author: Daniel Kuehlwein
+'''
+
+def eval_against_dataset(dataset, scheduler):
+    '''
+    nr_of_problems = feature_matrix.shape(0) 
+    
+    for _i in range()
+    self.feature_matrix = None
+    self.strategy_matrix = None
+    self.strategies = None
+    self.problems = None
+    '''
+    pass
+    
+    
