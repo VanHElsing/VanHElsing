62404bfe53c47c5b6c600be6bcde6633cd275895
dkuehlwein <daniel.kuehlwein@gmail.com>
2014-05-17 19:41:55 +0200

    Evaluations now also compute the score (aka how much time is left).

--- src/eval/evaluations.py
+++ src/eval/evaluations.py
@@ -8,26 +8,30 @@ import numpy as np
 
 
 def eval_against_dataset(dataset, scheduler, max_time=300):
-    problems_solved = 0.0
+    schedule_solved = 0.0
     problems_solvable = 0.0
+    schedule_score = 0.0
+    best_score = 0.0
 
     for p_index, problem in enumerate(dataset.problems):
-        p_time = max_time
+        time_left = max_time
         p_features = dataset.feature_matrix[p_index, :]
         p_times = dataset.strategy_matrix[p_index, :]
         min_strat_time = np.ma.masked_equal(p_times, -1, copy=False).min()
         if min_strat_time < max_time:
             problems_solvable += 1
+            best_score += (max_time - min_strat_time)
         scheduler.set_problem_and_features(problem, p_features)
-        while p_time > 0:
-            strat, strat_time = scheduler.predict(p_time)
-            run_time = min(strat_time, p_time)
-            p_time -= run_time
+        while time_left > 0:
+            strat, strat_time = scheduler.predict(time_left)
+            run_time = min(strat_time, time_left)
+            time_left -= run_time
             strat_index = np.where(dataset.strategies == strat)[0][0]
             strat_time = dataset.strategy_matrix[p_index, strat_index]
             if strat_time == -1:
                 continue
             if run_time >= strat_time:
-                problems_solved += 1
+                schedule_solved += 1
+                schedule_score += (time_left + run_time - strat_time)
                 break
-    return problems_solved / problems_solvable
+    return schedule_solved / problems_solvable, schedule_score / best_score
--- src/learn.py
+++ src/learn.py
@@ -97,25 +97,26 @@ def main(argv=sys.argv[1:]):
             LOGGER.info("Fitting model.")
             scheduler.fit(train_dataset, max_time)
             LOGGER.info("Evaluating model.")
-            score = eval_against_dataset(test_dataset, scheduler)
+            solved, _score = eval_against_dataset(test_dataset, scheduler)
             try:
-                sumscore += score
+                sumscore += solved
             except TypeError:
                 LOGGER.warn("Evaluation score is not a number.")
-                score = 0
-            LOGGER.info("Score: {}".format(score))
+                solved = 0
+            LOGGER.info("Solved: {}".format(solved))
         LOGGER.info("Total score: {}".format(sumscore / kfolds))
     if eval_whole or export_model:
         LOGGER.info("TRAINING (Whole dataset):")
         scheduler.fit(dataset, max_time)
         if eval_whole:
             LOGGER.info("EVALUATING (Whole dataset):.")
-            score = eval_against_dataset(dataset, scheduler)
+            solved, score = eval_against_dataset(dataset, scheduler)
             try:
-                score += 0
+                solved += 0
             except TypeError:
                 LOGGER.warn("Evaluation score is not a number.")
-                score = 0
+                solved = 0
+            LOGGER.info("Solved: {}".format(solved))
             LOGGER.info("Score: {}".format(score))
         if export_model:
             exportfile = args.outputfile
