78819fc9881eb2502b832eb6ec8ebfe5bc26785e
dkuehlwein <daniel.kuehlwein@gmail.com>
2014-05-18 17:05:15 +0200

    More Todos

---
+++ src/CPU.py
@@ -0,0 +1,8 @@
+'''
+Created on May 18, 2014
+
+@author: daniel
+'''
+
+
+# TODO: Measure CPU Speed
--- src/eval/atp_evaluations.py
+++ src/eval/atp_evaluations.py
@@ -3,3 +3,6 @@ Created on May 17, 2014
 
 @author: daniel
 '''
+
+
+# TODO: Take E-MaLeS 1.2 test problems. Run ATP over them. Store and compare.
