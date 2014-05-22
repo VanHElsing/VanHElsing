e0c4dbd2bb019551310e844fde163fa486afc83b
dkuehlwein <daniel.kuehlwein@gmail.com>
2014-05-22 16:35:28 +0200

    Added basic greedy scheduler for combinations with other schedulers. Added new results for NN scheduler

--- src/eval/plot_results.py
+++ src/eval/plot_results.py
@@ -73,12 +73,14 @@ def plot_real_training():
     e18 = (os.path.join(path, 'atp_eval_CASC_Train_E1.8'), 'E 1.8')
     e17 = (os.path.join(path, 'atp_eval_CASC_Train_E1.7'), 'E 1.7')
     emales = (os.path.join(path, 'atp_eval_CASC_Train_emales1.2'), 'E-MaLeS 1.2')
-    helsing_nn = (os.path.join(path, 'atp_eval_CASC_Train_helsing_NN_with_basic_time_scaling'), 'Hesling NN')
+    helsing_nn_wts = (os.path.join(path, 'atp_eval_CASC_Train_helsing_NN_with_basic_time_scaling'), 'Hesling NN wts')
+    helsing_nn_wots = (os.path.join(path, 'atp_eval_CASC_Train_helsing_NN_without_time_scaling'), 'Hesling NN wots')    
     result_tuples = []
     result_tuples.append(e18)
     result_tuples.append(e17)
     result_tuples.append(emales)
-    result_tuples.append(helsing_nn)
+    result_tuples.append(helsing_nn_wts)
+    result_tuples.append(helsing_nn_wots)
     return result_tuples, axis_vals
 
 
---
+++ src/schedulers/GreedyScheduler.py
@@ -0,0 +1,61 @@
+'''
+Created on May 22, 2014
+
+@author: Daniel Kuehlwein
+'''
+
+import copy
+import numpy as np
+from src.schedulers.SchedulerTemplate import StrategyScheduler
+from src.data_util import remove_unsolveable_problems, not_solved_by_strat
+
+
+class GreedyScheduler(StrategyScheduler):
+
+    def __init__(self, config):
+        """
+        config: Load settings from a SafeConfigParser instance
+        """
+        StrategyScheduler.__init__(self, config)
+        self.data_set = None
+        self._data_set = None
+        self.problem = None
+        self.features = None
+        self.last_strategy = None
+        self.last_time = None
+        
+    def fit(self, data_set, max_time):
+        self._data_set = remove_unsolveable_problems(data_set)
+        self.data_set = copy.deepcopy(self._data_set)
+
+    def predict(self, time_left, run_time = None):
+        if run_time is None:
+            run_time = 1.0
+        solved_in_run_time = np.array([0] * self.data_set.strategy_matrix.shape[1])
+        # TODO: there has to be a better way to do this
+        problems = self.data_set.strategy_matrix.shape[0]
+        strategies = self.data_set.strategy_matrix.shape[1]
+        for i in range(problems):
+            for j in range(strategies):
+                if -1 < self.data_set.strategy_matrix[i,j] < run_time:
+                    solved_in_run_time[j] += 1
+        self.last_strategy = solved_in_run_time.argmax()
+        self.last_time = run_time
+        strategy = self.data_set.strategies[self.last_strategy]
+        return strategy, run_time
+
+    def reset(self):
+        self.data_set = copy.deepcopy(self._data_set)
+
+    def set_problem(self, problem_file):
+        self.problem = problem_file        
+
+    def set_problem_and_features(self, problem_file, problem_features):
+        self.problem = problem_file
+        self.features = problem_features
+
+    def update(self):
+        good_problems = not_solved_by_strat(self.data_set, self.last_strategy, self.last_time)
+        # TODO: Have to check that we do not run out of options
+        self.data_set = self.data_set.mask(good_problems)
+
