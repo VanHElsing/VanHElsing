838cc03e80975dd0be3d668a8669a90044025f93
Wouter Geraedts <git@woutergeraedts.nl>
2014-08-03 21:40:53 +0200

    data_util: improved readability

--- src/data_util.py
+++ src/data_util.py
@@ -14,8 +14,7 @@ def remove_unsolveable_problems(data_set):
     Deletes all problems that cannot be solved.
     """
     problem_filter = np.max(data_set.strategy_matrix, axis=1) > -1
-    ret = data_set.mask(problem_filter)
-    return ret
+    return data_set.mask(problem_filter)
 
 
 def not_solved_by_strat(data_set, s_index, s_time):
@@ -24,10 +23,10 @@ def not_solved_by_strat(data_set, s_index, s_time):
     by strategy s_index in s_time.
     """
     nr_of_problems = data_set.strategy_matrix.shape[0]
-    s_m = data_set.strategy_matrix
+    s_matrix = data_set.strategy_matrix
     not_solved_by_strats = [i for i in range(nr_of_problems) if
-                            (s_m[i, s_index] > s_time or
-                             s_m[i, s_index] == -1)]
+                            (s_matrix[i, s_index] > s_time or
+                             s_matrix[i, s_index] == -1)]
     return not_solved_by_strats
 
 
